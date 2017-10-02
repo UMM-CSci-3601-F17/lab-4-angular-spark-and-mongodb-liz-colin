d521fc9b2da88c2c71244edeb1d277dbf7e6664c
rabex028 <rabex028@morris.umn.edu>
2017-10-02 13:55:49 -0500

    Including other files from previous commit

--- client/src/app/todos/todo-list.service.ts
+++ client/src/app/todos/todo-list.service.ts
@@ -11,15 +11,60 @@ import {environment} from "../../environments/environment";
 @Injectable()
 export class TodoListService {
     private todoUrl: string = environment.API_URL + "todos";
+    public serviceOwner: string;
+    public serviceStatus: string;
+    public serviceContent: string;
 
     constructor(private http: Http) {
     }
 
-    getTodos(): Observable<Todo[]> {
+
+
+    getTodos(): Observable<any> {
+        this.todoUrl = environment.API_URL + "todos";
+        if (this.serviceOwner !== "") {
+            console.log("owner specified");
+
+            if (this.todoUrl.indexOf('&') !== -1) {
+                this.todoUrl += 'owner=' + this.serviceOwner + '&';
+            }
+            else {
+                this.todoUrl += "?owner=" + this.serviceOwner + "&";
+            }
+        }
+        if (this.serviceStatus !== "") {
+            console.log("status specified");
+            if (this.todoUrl.indexOf('&') !== -1) {
+                this.todoUrl += 'status=' + this.serviceStatus + '&';
+            }
+            else {
+                this.todoUrl += "?status=" + this.serviceStatus + "&";
+            }
+        }
+
+        if (this.serviceContent !== "") {
+            if (this.todoUrl.indexOf('&') !== -1) {
+                this.todoUrl += 'content=' + this.serviceContent + '&';
+            }
+            else {
+                this.todoUrl += "?content=" + this.serviceContent + "&";
+            }
+            console.log(this.todoUrl);
+        }
+
         let observable: Observable<any> = this.http.request(this.todoUrl);
         return observable.map(res => res.json());
     }
 
+
+
+
+    /*
+    getTodos(): Observable<Todo[]> {
+        let observable: Observable<any> = this.http.request(this.todoUrl);
+        return observable.map(res => res.json());
+    }
+*/
     getTodoById(id: string): Observable<Todo> {
         return this.http.request(this.todoUrl + "/" + id).map(res => res.json());
     }
